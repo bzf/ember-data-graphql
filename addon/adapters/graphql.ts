0ff5eaddc589d78d4b95c606e8aec3567feb7c55
André Ligné <andre@teamtailor.com>
2021-05-11 15:38:15 +0200

    Add support for store.queryRecord

--- addon/adapters/graphql.ts
+++ addon/adapters/graphql.ts
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import gql from 'graphql-tag';
 import { isPresent } from '@ember/utils';
 import { print } from 'graphql/language/printer';
-import { pluralize } from 'ember-inflector';
+import { pluralize, singularize } from 'ember-inflector';
 
 export default class Graphql extends DS.Adapter {
   host = undefined;
@@ -46,8 +46,54 @@ export default class Graphql extends DS.Adapter {
     }
   }
 
+  async queryRecord(store, type, options) {
+    const serializer = store.serializerFor(type.modelName);
+    const modelClass = store.modelFor(type.modelName);
+
+    const url = [this.host, this.namespace, 'graphql']
+      .filter(isPresent)
+      .join('/');
+
+    const fieldName = this.fieldForQueryRecord(type);
+    const fragmentName = shortId();
+    const fragment = serializer.fragment(modelClass, fragmentName);
+
+    console.log(fragment);
+
+    const query = gql`
+      query {
+        ${fieldName}${this.serializeParams(options)} {
+          ...${fragmentName}
+          id
+          __typename
+        }
+      }
+      ${fragment}
+    `;
+
+    console.log(query);
+
+    const result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: `query ${print(query)}` }),
+    }).then((r) => r.json());
+
+    if (Array.isArray(result.data[fieldName])) {
+      return result.data[fieldName].firstObject;
+    } else {
+      return result.data[fieldName];
+    }
+  }
+
   fieldForQuery(modelClass) {
     return pluralize(modelClass.modelName).toLowerCase();
+  }
+
+  fieldForQueryRecord(modelClass) {
+    return singularize(modelClass.modelName).toLowerCase();
   }
 
   // * `createRecord()`
--- tests/unit/adapters/graphql-test.ts
+++ tests/unit/adapters/graphql-test.ts
@@ -26,7 +26,6 @@ module('Unit | Adapter | graphql', function (hooks) {
 
     const store = this.owner.lookup('service:store');
     const adapter = this.owner.lookup('adapter:graphql');
-    // this.owner.register('serializer:graphql', GraphqlSerializer);
 
     class User extends Model {
       modelName = 'user';
@@ -45,6 +44,42 @@ module('Unit | Adapter | graphql', function (hooks) {
     assert.deepEqual(result, [
       { id: 1, name: 'Bruno', age: 35, __typename: 'User' },
     ]);
+  });
+
+  test('queryRecord', async function (assert) {
+    this.server.post(
+      '/graphql',
+      () => ({
+        data: {
+          users: { id: 1, name: 'Bruno', age: 35, __typename: 'User' },
+        },
+      }),
+      200
+    );
+
+    const store = this.owner.lookup('service:store');
+    const adapter = this.owner.lookup('adapter:graphql');
+
+    class User extends Model {
+      modelName = 'user';
+
+      @attr('string') declare name: string;
+      @attr('number') declare age: number;
+    }
+
+    User.modelName = 'user';
+
+    this.owner.register('model:user', User);
+    this.owner.register('serializer:user', GraphqlSerializer);
+
+    const result = await adapter.queryRecord(store, User, {}, []);
+
+    assert.deepEqual(result, {
+      id: 1,
+      name: 'Bruno',
+      age: 35,
+      __typename: 'User',
+    });
   });
 
   test('serializeParams', function (assert) {
