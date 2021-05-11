ce494b40387ebd47ee3cd14b33ace24b836912a3
André Ligné <andre@teamtailor.com>
2021-05-11 14:21:39 +0200

    Disable mirage outside of tests

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -23,6 +23,10 @@ module.exports = function (environment) {
     },
   };
 
+  ENV['ember-cli-mirage'] = {
+    enabled: environment === 'test',
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
